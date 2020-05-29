b3ddb22f575c28c3af42a3ef2f4f23823e86d157
1602762618 <1602762618@qq.com>
2020-05-29 09:15:11 +0800

    李鹏第一次提交

--- mytest/src/com/dong1990/MyTest.java
+++ mytest/src/com/dong1990/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("哈哈");
         System.out.println("are you ready?");
+        System.out.println("lllll");
     }
 }
